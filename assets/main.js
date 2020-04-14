b2bbb9a86937f658846361a620e5e12e08f6c246
MarcoMadeddu <marcomade99@gmail.com>
2020-04-14 19:19:37 +0200

    first version

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
-console.log(jQuery);
+
+var menuDropdown = $(".menu-dropdown");
+var showDropdown = $(".f-dropdown");
+
+showDropdown.mouseenter(function(){
+
+  var current = $(this).next(".menu-dropdown");
+  current.addClass("flex-active");
+  console.log(current);
+
+});
+
+showDropdown.mouseleave(function(){
+  var currentDropdown = $(this).next('li ul');
+  currentDropdown.removeClass("flex-active");
+});
